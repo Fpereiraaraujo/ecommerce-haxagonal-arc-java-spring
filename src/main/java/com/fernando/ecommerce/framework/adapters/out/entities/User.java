c491ea3dc1bfc8681e1d1ba08d6b0db0ed3f128e
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-01-11 21:36:38 -0300

    criando estrutura dos arquivos

---
+++ src/main/java/com/fernando/ecommerce/framework/adapters/out/entities/User.java
@@ -0,0 +1,35 @@
+package com.fernando.ecommerce.framework.adapters.out.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "type")
+    private String type;
+}
